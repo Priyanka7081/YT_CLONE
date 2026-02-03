f1a50ebe2885af6e48bbabe43f8e9d5389edbc11
Priyanka7081 <priyankau7081@gmail.com>
2026-02-03 15:56:58 +0530

    YOUTUBE CLONE

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Sidebar from './Components/Sidebar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Sidebar />
+    </>
+  )
+}
+
+export default App
